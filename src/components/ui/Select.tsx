d9b3e1cfac8df9a3275eb0a4462193ab93b36d96
Grupo RaVal <gruporaval@MacBook-Pro-de-Grupo.local>
2026-02-05 01:33:59 -0300

    Fix: Melhorar filtro de opções vazias no Select

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -64,7 +64,7 @@ export const Select = ({
           >
             <SelectPrimitive.Viewport className="p-1">
               {options
-                .filter((option) => option.value !== '') // Filtrar opções com valor vazio
+                .filter((option) => option.value != null && option.value !== '') // Filtrar opções com valor vazio, null ou undefined
                 .map((option) => (
                 <SelectPrimitive.Item
                   key={option.value}
